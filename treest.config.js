55fdc1731b9fe694535f31f741b048b791989cba
Vitaly Arkhipov <arvitaly@list.ru>
2018-04-19 13:34:05 +0300

    updated tests

--- treest.config.js
+++ treest.config.js
@@ -1,11 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const fixtures_1 = require("./fixtures");
-const fixtures = [fixtures_1.test1, fixtures_1.test2, fixtures_1.test3, fixtures_1.test4];
-exports.default = {
-    tests: fixtures.map((f) => ({
-        module: "./index",
-        exportName: "default",
-        args: [f],
-    })),
-};
+exports.default = {};
--- treest.config.ts
+++ treest.config.ts
@@ -1,9 +1,3 @@
-import { test1, test2, test3, test4 } from "./fixtures";
-const fixtures = [test1, test2, test3, test4];
 export default {
-    tests: fixtures.map((f) => ({
-        module: "./index",
-        exportName: "default",
-        args: [f],
-    })),
+
 };
