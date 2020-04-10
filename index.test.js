c81a462118ec22dacdff0761db43169fd0850de4
Administrator <admin@AirAdmiistrator.domain.name>
2020-04-10 11:30:23 +0800

    up deps

--- index.test.js
+++ index.test.js
@@ -1,9 +1,10 @@
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
@@ -13,7 +14,7 @@ it("when withError callback non-promisify and not thrown exception, should retur
     const [result] = _1.default(() => "test");
     expect(result.toUpperCase()).toBe("TEST");
 });
-it("when withError callback is promisify and not throw exception, should return promise with result", () => __awaiter(this, void 0, void 0, function* () {
+it("when withError callback is promisify and not throw exception, should return promise with result", () => __awaiter(void 0, void 0, void 0, function* () {
     const { result } = yield _1.default(() => Promise.resolve("test2"));
     expect(result.toUpperCase()).toBe("TEST2");
 }));
@@ -23,7 +24,7 @@ it("when withError callback non-promisify and thrown exception, should return er
     });
     expect(error).toEqual(new Error("Error1"));
 });
-it("when withError callback promisify and thrown exception, should return error", () => __awaiter(this, void 0, void 0, function* () {
+it("when withError callback promisify and thrown exception, should return error", () => __awaiter(void 0, void 0, void 0, function* () {
     const { error } = yield _1.default(() => Promise.reject(new Error("Error2")));
     expect(error).toEqual(new Error("Error2"));
 }));
